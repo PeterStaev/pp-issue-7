96d14fad637372e7be9a4e628b26dbce602f291d
Peter Staev <peter@tangrasoft.com>
2020-11-20 15:40:23 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular/nativescript.module";
+import { DropDownModule } from "@proplugins/nativescript-drop-down/angular";
+
+import { AppRoutingModule } from "./app.routing";
+import { AppComponent } from "./app.component";
+
+import { DropDownComponent } from "./dropdown/dropdown.component";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        DropDownModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        DropDownComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
